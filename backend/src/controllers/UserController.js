53e0d28556877cee8fff3658640b421549759116
Waifu_Forever <itachiut1r4@gmail.com>
2021-02-23 15:49:53 -0300

    user model/controller

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,147 @@
+import * as mongoose from 'mongoose';
+import * as dotenv from "dotenv";
+import * as bcrypt from 'bcrypt';
+
+import { Request, Response, NextFunction } from 'express';
+
+import User from '../models/user';
+
+import response from '../common/response';
+
+import jwt from '../common/jwt';
+
+dotenv.config();
+
+
+
+
+module.exports = {
+    async store(req, res){   
+                       
+        const { email, password } = req.body;
+    
+        if(email && password){                       
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+            
+            const p1 = new User ({
+                email: email,
+                password: hash
+                
+            });
+
+            p1.save().then(result => {
+
+                const token = jwt.generateJwt({id: p1._id});
+                const refreshToken = jwt.generateRefreshJwt({id: p1._id});
+
+                res.json(        
+                    response.jsonOK(result, response.getMessage("user.valid.sign_up.sucess"), {token, refreshToken})              
+                );                              
+
+            }).catch(err => {
+                
+                console.log(err)
+                if (err.name === 'MongoError' && err.code === 11000) {
+                    //next(new Error('There was a duplicate key error'));
+                    return res.json(        
+                        response.jsonBadRequest(null, "There was a duplicate key error", {err})              
+                    );  
+                
+                } else {
+                    return res.json(        
+                        response.jsonBadRequest(null, null, {err})              
+                    );  
+                
+                }       
+                    
+            });     
+        
+        } 
+        else{
+            return res.json(        
+                response.jsonBadRequest(null, response.getMessage("badRequest"), null)              
+            ); 
+        }                                                                     
+    },
+
+    async index(req, res){
+        
+        const { email } = req.query;
+       
+        let docs = [];
+
+        if (email){
+            (await User.find( {email: {$regex: email, $options: "i"} } )).forEach(function (doc){
+                docs.push(doc)
+            });
+        }
+
+
+        else{
+            (await User.find()).forEach(function (doc){
+                docs.push(doc)
+            });     
+        }
+          
+        
+        
+
+        console.log(docs)
+        res.json(        
+            response.jsonOK(docs, `Page list retrieved successfully! Users found: ${docs.length}`, null)              
+        );
+    },
+
+    async delete(req, res){
+    
+        const { user_id } = req;
+        console.log(user_id)
+
+        User.findOne({ _id: user_id }).then(result =>  {
+            User.deleteOne({ _id: user_id }, function (err) {
+                if (err){
+                    return res.json(        
+                        response.jsonNotFound(null, "The specified user could not be deleted", err.message)              
+                    )  
+                } else {
+                    return res.json(        
+                        response.jsonOK(null, "The specified user was deleted", result)              
+                    )  
+                  }
+                   
+                // deleted at most one tank document
+                });  
+        }).catch(err => {
+            return res.json(        
+                response.jsonNotFound(null, "The specified user could not be found", err.message)              
+            )             
+        })
+
+             
+    },
+
+    async auth(req, res){
+        const { email, password } = req.body;      
+
+        const user = await User.findOne({ email: email }).select('password')
+
+        const match = user ? await bcrypt.compare(password, user.password) : null;
+        
+        if(!match){
+            return res.json(
+                response.jsonBadRequest(null, "Bad Request", null)
+            )
+        } else{
+            const token = jwt.generateJwt({id: user._id});
+            const refreshToken = jwt.generateRefreshJwt({id: user._id});
+           
+            return res.json(
+                response.jsonOK(user, response.getMessage("user.valid.sign_in.sucess"), {token, refreshToken})
+            )
+        }
+
+           
+        
+    }
+}
---
+++ backend/src/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required:true, select: false },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+    addedAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+    stocks: [{ //a array fill with the stocks ids
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Stock'
+    
+    }],
+});
+
+export default mongoose.model('User', UserSchema);
