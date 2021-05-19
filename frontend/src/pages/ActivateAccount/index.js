72ad48927a27f84f5f337f2b28670ceead2a17a0
Waifu_Forever <itachiut1r4@gmail.com>
2021-05-18 23:49:08 -0300

    axios

---
+++ frontend/src/pages/ActivateAccount/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Home(){
+    return(
+        <>  
+            <div className="home-container">
+                <h1>HOME</h1>
+            </div>
+            
+        </>
+    )
+}
