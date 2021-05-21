5ca922d8b250a7de99c092579076daba6571c031
Waifu_Forever <itachiut1r4@gmail.com>
2021-05-21 20:51:19 -0300

    roles permission

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -5,31 +5,37 @@ const User = require("../models/user");
 
 module.exports = {
   
-    async auth(req, res, next){
+    async auth(req, res, next, role){
         try{
             const [, token] = req.headers.authorization.split(" ")
             
             try{
                 const payload = await jwt.verifyJwt(token, 1);
-                                
-                if(await User.exists({_id: payload.id, active: true})){    
-                    try{                                
-                        req.auth = require("crypto-js").AES.encrypt(payload.id, `${process.env.SHUFFLE_SECRET}`);
-                        next();
-                        
-                    }   catch(err){
-                        console.log(err)
-                        return res.json(
+                if (payload.role === role){
+                    if(await User.exists({_id: payload.id, active: true})){    
+                        try{                                
+                            req.auth = require("crypto-js").AES.encrypt(payload.id, `${process.env.SHUFFLE_SECRET}`);
+                            next();
+                            
+                        }   catch(err){
+                            console.log(err)
+                            return res.json(
+                                response.jsonUnauthorized(null, response.getMessage("Unauthorized"), null)
+                            )
+                        }           
+                     
+                    }
+                    else{ 
+                        return res.json( 
                             response.jsonUnauthorized(null, response.getMessage("Unauthorized"), null)
                         )
-                    }           
-                 
-                }
-                else{ 
+                    }
+                } else{
                     return res.json( 
                         response.jsonUnauthorized(null, response.getMessage("Unauthorized"), null)
                     )
                 }
+              
 
             } catch(err){
                 return res.json(
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -30,19 +30,19 @@ routes.put("/authentication/recover/:tky", AuthController.recoverPassword);// PU
 
 routes.post('/sign-up', UserMiddleware.valid_sign_up, UserController.store);
 routes.get('/sign-in', UserMiddleware.valid_sign_in, AuthController.sign_in);
-routes.get('/user/index', AuthMiddleware.auth, UserController.index);
-routes.put('/user/update', AuthMiddleware.auth, UserController.update)
-routes.delete('/user/delete', AuthMiddleware.auth, UserController.delete);
+routes.get('/user/index', AuthMiddleware.auth(req, res, next, "User"), UserController.index);
+routes.put('/user/update', AuthMiddleware.auth(req, res, next, "User"), UserController.update)
+routes.delete('/user/delete', AuthMiddleware.auth(req, res, next, "User"), UserController.delete);
 
 
-routes.post('/manga/post', AuthMiddleware.auth, MangaMiddleware.valid_manga_store, MangaController.store);
-routes.get('/manga/index', AuthMiddleware.auth, MangaController.index);
-routes.put('/manga/update', AuthMiddleware.auth, MangaController.update);
-routes.delete('/manga/delete', AuthMiddleware.auth, MangaMiddleware.valid_manga_delete, MangaController.delete);
+routes.post('/manga/post', AuthMiddleware.auth(req, res, next, "Scan"), MangaMiddleware.valid_manga_store, MangaController.store);
+routes.get('/manga/index', AuthMiddleware.auth(req, res, next, "Scan"), MangaController.index);
+routes.put('/manga/update', AuthMiddleware.auth(req, res, next, "Scan"), MangaController.update);
+routes.delete('/manga/delete', AuthMiddleware.auth(req, res, next, "Scan"), MangaMiddleware.valid_manga_delete, MangaController.delete);
 
 
-routes.post('/chapter/post', AuthMiddleware.auth, multer(multerConfig).array('imgCollection'), ChapterController.store);
-routes.get('/chapter/index', AuthMiddleware.auth, (req, res) => {
+routes.post('/chapter/post', AuthMiddleware.auth(req, res, next, "Scan"), multer(multerConfig).array('imgCollection'), ChapterController.store);
+routes.get('/chapter/index', AuthMiddleware.auth(req, res, next, "Scan"), (req, res) => {
     ChapterController.index();
    
 })
