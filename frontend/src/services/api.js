cc0fcebd4b3103efb081f2ebfff2112d43895fa8
WaifuForever <itachiut1r4@gmail.com>
2021-06-05 19:27:27 -0300

    axios update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
 
+var token = null
 
-const api = axios.create({
-    baseURL: 'http://localhost:3333',
-});
+module.exports = {
+    get_instance(){
+        return axios.create({
+            baseURL: 'http://localhost:3333',
+        });
+    },
 
-export default api;
+    get_token(new_token){
+        if(new_token){
+            token = new_token
+            return new_token
+        }
+        return token    
+        
+    }
+
+}
+
+
