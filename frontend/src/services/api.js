872875eac34ba79a89f7d0a8d18fd8f515fb1333
WaifuForever <itachiut1r4@gmail.com>
2021-06-06 18:55:50 -0300

    axios update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,15 +4,4 @@ const api = axios.create({
     baseURL: 'http://localhost:3333',
 });
 
-var token = null
-
-export function getToken(new_token){
-    if(new_token){
-        token = new_token
-        return new_token
-    }
-    return token    
-}
-
-
 export default api;
