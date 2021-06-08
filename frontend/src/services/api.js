1429e34f8fe93cd6fac5bcce96c58318df1b5943
WaifuForever <itachiut1r4@gmail.com>
2021-06-07 21:14:34 -0300

    axios config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'http://localhost:3333'
+    
+    
 });
 
+api.defaults.withCredentials = true
 export default api;
