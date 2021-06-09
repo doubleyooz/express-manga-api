c71fab7383f33ea8c09d2057d23f9bbfc73980ae
WaifuForever <itachiut1r4@gmail.com>
2021-06-09 20:09:04 -0300

    cors headers

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,15 +30,15 @@ const allowedOrigins = ['http://localhost:3000']
 
 const headers = [ 
  "Origin", "Access-Control-Allow-Origin", "Content-Type",
- /*"Accept",*/ "Authorization", "Origin", /*"Accept"*/, "X-Requested-With",
- /*"Access-Control-Request-Method", "Access-Control-Request-Header"*/
+ "Accept", "Authorization", "Origin", "X-Requested-With",
+ /*"Access-Control-Request-Method",*/ "Access-Control-Allow-Credentials", /*"Access-Control-Request-Header"*/
 ]
 var corsOptionsDelegate = function (req, callback){
   
     var corsOptions;
     if (allowedOrigins.indexOf(req.header('Origin')) !== -1){
       corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-      console.log("origin allowed")
+      
     }     
     else {
       corsOptions = { origin: false } // disable CORS for this request    
@@ -46,7 +46,7 @@ var corsOptionsDelegate = function (req, callback){
     }
      
    
-    //corsOptions.allowedHeaders = headers,
+    corsOptions.allowedHeaders = headers,
     corsOptions.methods = ['GET', 'PUT', 'POST', 'DELETE']    
     corsOptions.credentials = true
     callback(null, corsOptions) // callback expects two parameters: error and options
