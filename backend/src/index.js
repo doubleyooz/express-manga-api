5bc6c027b0009c122ca73451e50ef184d303d69f
WaifuForever <itachiut1r4@gmail.com>
2021-06-09 19:08:13 -0300

    cors configured

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,8 +11,7 @@ const server = require('http').Server(app);
 //const io = require('socket.io')(server);
 
 
-const allowedOrigins = ['http://localhost:3000'] 
-  
+
 
 mongoose.connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-c09yq.mongodb.net/MangaReader?retryWrites=true&w=majority`, {
@@ -26,27 +25,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static('uploads'));
 
-app.use(cookieParser())
-app.use((req, res, next)=>{    
+
+const allowedOrigins = ['http://localhost:3000']
+
+const headers = [ 
+ "Origin", "Access-Control-Allow-Origin", "Content-Type",
+ /*"Accept",*/ "Authorization", "Origin", /*"Accept"*/, "X-Requested-With",
+ /*"Access-Control-Request-Method", "Access-Control-Request-Header"*/
+]
+var corsOptionsDelegate = function (req, callback){
+  
+    var corsOptions;
+    if (allowedOrigins.indexOf(req.header('Origin')) !== -1){
+      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+      console.log("origin allowed")
+    }     
+    else {
+      corsOptions = { origin: false } // disable CORS for this request    
+      console.log("Origin not allowed")
+    }
+     
+   
+    //corsOptions.allowedHeaders = headers,
+    corsOptions.methods = ['GET', 'PUT', 'POST', 'DELETE']    
+    corsOptions.credentials = true
+    callback(null, corsOptions) // callback expects two parameters: error and options
     
-    res.header("Access-Control-Allow-Headers", "*")
-    app.use(cors({
-        origin: function(origin, callback){
-          // allow requests with no origin 
-          // (like mobile apps or curl requests)
-          if(!origin) return callback(null, true);
-          if(allowedOrigins.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not ' +
-                      'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-          }
-          return callback(null, true);
-        },
-        credentials: true,
-      }));
-    //req.io = io;
-    next();
-});
+  
+}
+
+
+ 
+app.use(cookieParser())
+
+app.use(cors(corsOptionsDelegate));
+
 
 
 
