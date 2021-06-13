524d4ed9904f3c839305d4c8d4d12b87153983b5
WaifuForever <itachiut1r4@gmail.com>
2021-06-12 22:21:27 -0300

    manga route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Reader from './pages/Reader'
 import ActivateAccount from './pages/ActivateAccount'
 import Home from './pages/Home'
-
+import Manga from './pages/Manga'
 
 import HorizontalMenu from './Components/Menu/Horizontal'
 
@@ -15,6 +15,7 @@ export default function Routes(){
         <BrowserRouter>
             
             <Switch>
+                <Route path="/manga" exact component ={Manga}/>
                 <Route path="/reader" exact component ={Reader}/>
                 <Route path="/" exact component ={Home}/>
                 <Route path="/activateaccount/:token" exact component ={ActivateAccount}/>
