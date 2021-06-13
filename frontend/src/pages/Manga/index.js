d3f1f75e370b1b135c516e4a1375678b7a5072bf
WaifuForever <itachiut1r4@gmail.com>
2021-06-12 22:21:45 -0300

    Manga page in progress

--- frontend/src/pages/Manga/index.js
+++ frontend/src/pages/Manga/index.js
@@ -1,163 +1,102 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 
 import './styles.css';
 
-import banner from "../../assets/banner_scan.png"
 
+import { Context } from '../../Contexts/AuthProvider'
 import api from "../../services/api"
+
+require('dotenv').config()
 
 export default function Manga(){
 
-    const dir = ""
-
-
-
-    const [currentPage, setCurrentPage] = useState({
-        id: 0,
-        path:  dir + 'banner_scan.png',
-    });  
+   
+    const { token, loading, handleLogin } = useContext(Context)
     
-    async function componentDidMount() {
-        //registerToSocket();
-        let config = {
-            
-            auth: {
-                username: "itachiut1r4@gmail.com",
-                password: "Lucarneiro@0009"
-            }
-            
-        }
+    
+    const [info, setInfo] = useState("")    
+    
+    
 
-       
+    let config = {
+        headers: {
+            'Authorization': `Bearer ${token}`
+          }
+    }
 
-        /*api.get('sign-in', config).then(response =>{
-            console.log(response.data)
-            console.log("login well succeed")
-        }).catch(err=>{
-            console.log(err)
-            console.log("login failed")
-        })*/
-        let token =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwYWM0MWRjMTE2NjVkMjg1NDk4N2M3MSIsInJvbGUiOiJTY2FuIiwiaWF0IjoxNjIxOTY0ODc3LCJleHAiOjE2MjE5NzIwNzd9.al2w6IMBeVcKkevHn94hQ0CffDkhh5sDl4t6Ou4fzYQ"
+    let manga_id = "60c3ff94f527df2248dcbff4"
+    useEffect(()=>{
+        async function selectedManga(){
+            api.get(`manga/list/manga_id=${manga_id}`, config)
+            .then(response => {
+                //setState({ feed: response.data });  
+                if(response.data !== null){
+                    console.log(response)
+                    console.log(response.data);
+                    
+                   
+                   
+                    setInfo(response.data[0])
+                    console.log("get manga info well succeed")
+                   
+                   
+                    
+                } else {
+                    console.log("get manga info failed")
+                    return null
+                }           
         
-        config = {
-            headers: {
-                Authorization: "Bearer " + token
-            }
-        }
-      
-      
-       
-        api.get('chapter/index?manga_id=60ac4bc918a0761d4c2babda&number=1', config).then(response => {
-            //setState({ feed: response.data });  
-            console.log(response.data);
-            console.log("list chapters well succeed")
-        }).catch(err =>{
-            console.log(err)
-            console.log("list chapters failed")
-        })
-      
-    }
-    componentDidMount()
-    
-    
-    var imagePath = [];
-    
-    function generatePath(someImageName) {
-        return require(`../../assets/${someImageName}`);
-    }
+            }).catch(err =>{
+                console.log(err)
+                console.log("get manga info failed")
+                return null
+            })        
+        }        
 
-    function getPaths(){
-        let temp = [ {
-            id: 0,
-            path: generatePath("banner_scan.png")
-        }];
+        async function fetchData(){
+            console.log(token)             
+             api.get('chapter/index?manga_id=60c3ff94f527df2248dcbff4&number=1', config)
+                .then(response => {
+                    //setState({ feed: response.data });  
+                    if(response.data !== null){
+                        console.log(response)
+                        console.log(response.data);
+                        console.log(response.data.data)
+                       
+                     
+                        setInfo({
+                            title: response.data.data[0].title,
+                            number: response.data.data[0].number
+                        })
+                        console.log("list chapters well succeed")
+                       
+                       
+                        
+                    } else {
+                        console.log("list chapters failed")
+                        return null
+                    }           
+            
+                }).catch(err =>{
+                    console.log(err)
+                    console.log("list chapters failed")
+                    return null
+                })        
+        }        
+        fetchData()     
+            
+    }, []) // <-- empty dependency array
 
-        for(var i = 1; i < 49; i++){
-            if (i < 10){
-                temp.push({
-                    id: i, 
-                    path: generatePath(`cap/0${i}.jpg`)
-                   
-                });                
-            }               
-            else {
-                temp.push({
-                    id: i, 
-                    path:  generatePath(`cap/${i}.jpg`)
-                })
-            }
-        imagePath = temp
-        }
-    }
-                   
-    
-    function nextPage(){    
-        getPaths()
-        imagePath.forEach((obj, index) => {
-            if(obj.id === currentPage.id){
-                console.log(imagePath.length)
-                console.log(index)
-                if(imagePath.length === index+1){
-
-                } else{
-                    setCurrentPage(imagePath[index+1])
-                }
-                
-            }
-        })
-        console.log(currentPage.path)
-     
-    }
-
-    function prevPage(){    
-        getPaths();
-        imagePath.forEach((obj, index) => {
-            if(obj.id === currentPage.id){
-                console.log(imagePath.length)
-                console.log(index)
-                if(index === 0){
-
-                } else{
-                    setCurrentPage(imagePath[index-1])
-                }
-                
-            }
-        })
-        console.log(currentPage.path)
-     
-    }
-
+  
     return <>   
         <div className="reader">
             <div className="header-board">
 
                     <div className="title">
-                        Rental Onii-chan!
+                        {info.title}
                     </div>
-                    <div className="controllers">
-                        <button className='button'>◄◄</button>
-                        <div className="chapters"> Chapter #06 </div>
-                        <button className='button'>►►</button>
-                   
-                   
-                    </div>
-                    <div className="controllers">
-                        <button className='button' onClick= {() => componentDidMount()}>prev</button>
-                        <div className="pages-list"> </div>
-                        <div className="version"></div>
-                        <div className="viewToggle"></div>
-                        <button className='button' onClick={() => nextPage()}>next</button>
-                   
-                   
-                    </div>
-            </div>
 
 
-
-            <div className='board'>       
-
-
-                <img className='page' src={currentPage.path} alt={currentPage.id} onClick={() => nextPage()}/>
             </div>
 
 
