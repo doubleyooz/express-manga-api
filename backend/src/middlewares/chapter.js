119aa45e8e27aefbb868cf59903a76b8000a4402
WaifuForever <itachiut1r4@gmail.com>
2021-06-20 15:31:55 -0300

    fixing chapter index middleware

--- backend/src/middlewares/chapter.js
+++ backend/src/middlewares/chapter.js
@@ -5,15 +5,10 @@ const { getMessage } = require("../common/messages");
 
 module.exports = {
     async valid_chapter_store(req, res, next){         
-                       
-        const { title, manga_id, number } = req.body;  
+        const { manga_id, number, title } = req.body;        
         
-        
-        console.log(title)
-
         let schema = yup.object().shape({
-            title: yup.string("title must be a string.").strict()
-                .min(2, getMessage("chapter.invalid.title.short"))
+            title: yup.string("title must be a string.").strict()                
                 .max(40, getMessage("chapter.invalid.title.long")),
             number: yup.number("Must to be a valid number").min(1, 'Must be a positive number').required(),
                                
@@ -22,7 +17,7 @@ module.exports = {
         try{
             await schema.validate({title, number})
             .catch(function(e) {
-                return res.jsonBadRequest(null, null, null)
+                return res.jsonBadRequest(null, null, e)
                 
             });
 
@@ -48,49 +43,135 @@ module.exports = {
                         
     },
 
-    async valid_manga_index(req, res, next){
-        
+    async valid_chapter_index(req, res, next){
+        const { manga_id, number, chapter_id } = req.query; 
+
         let schema = yup.object().shape({
-            title: yup.string("title must be a string.").strict(),
-            genre: yup.string("genre must be a string.").strict(),
-            scan: yup.string("scan must be a string.").strict(),
+            number: yup.number("Must to be a valid number").min(1, 'Must be a positive number'),
+            manga_id: yup.string("genre must be a string.").strict().required(),
+           
 
         })
 
-        try{
-            schema.validate(req.query).then(() => next())
-            .catch((err) => {
-               return res.jsonBadRequest(null, null, err.errors)              
-               
-           })
-           
 
-        } catch(err){
-            return res.jsonBadRequest(null, null, err.errors)
+        let promises = []
+       
+        if (number){
+            promises.push(
+                schema.validate({number, manga_id}).then().catch(err => {                    
+                    return res.jsonBadRequest(null, null, err.errors)
+                })
+            )
+            
+        }     
+
+        if (chapter_id){
+            promises.push(
+                new Promise((resolve, reject) => {
+                    console.log("chapter_id2")
+                    if (mongoose.Types.ObjectId.isValid(chapter_id)) {   
+                        if (String(new mongoose.Types.ObjectId(chapter_id)) === chapter_id) {  
+                                
+                        } else { 
+
+                            return res.jsonBadRequest(
+                                    null,
+                                    getMessage("chapter.invalid.id"),
+                                    null
+                            );
+                                
+                        } 
+                    } else { 
+
+                        return res.jsonBadRequest(
+                                null,
+                                getMessage("chapter.invalid.id"),
+                                null
+                        );
+                            
+                    } 
+                })
+                
+            )
+  
         }
+
+        if (manga_id){
+            promises.push(
+                new Promise((resolve, reject) => {
+                    if (mongoose.Types.ObjectId.isValid(manga_id)) {   
+                        if (String(new mongoose.Types.ObjectId(manga_id)) === manga_id) {  
+                            console.log("manga id valid")
+                        } else{
+                            return res.jsonBadRequest(
+                                null,
+                                getMessage("manga.invalid.id"),
+                                null
+                            );
+                        }
+                    }   else{
+                        return res.jsonBadRequest(
+                            null,
+                            getMessage("manga.invalid.id"),
+                            null
+                        );
+                    }
+                  })
+                
+            )
+        }
+          
+        else{
+            return res.jsonBadRequest(null, null, null) 
+
+        }
+
+        Promise.all(promises).then(() => {            
+            next()            
+        }).catch(err => {
+            console.log("catch")
+            return res.jsonBadRequest(null, null, err)       
+        });
        
     },
 
-    async valid_manga_delete(req, res, next){         
+    async valid_chapter_delete(req, res, next){         
                        
-        const { manga_id } = req.query;       
+        const { manga_id, chapter_id } = req.query;       
     
-        if (mongoose.Types.ObjectId.isValid(manga_id)) {   
-            if (String(new mongoose.Types.ObjectId(manga_id)) === manga_id) {  
-                next();     
-            } else { 
+        try{
+            if (mongoose.Types.ObjectId.isValid(manga_id)) {   
+                if (String(new mongoose.Types.ObjectId(manga_id)) === manga_id) {  
+                    if (mongoose.Types.ObjectId.isValid(chapter_id)) {   
+                        if (String(new mongoose.Types.ObjectId(chapter_id)) === chapter_id) {  
+                            next();     
+                        } else { 
+        
+                        return res.jsonBadRequest(
+                                null,
+                                getMessage("chapter.invalid.id"),
+                                null);
+                            
+                        } 
+                    }
+                } 
+                else { 
 
-                return res.jsonBadRequest(
-                        null,
-                        getMessage("manga.invalid.id"),
-                        null);
-               
-            } 
-        } else {
-            return res.jsonBadRequest(
-                    null,
-                    getMessage("manga.invalid.id"),
-                    null);         
-        }                     
+                    return res.jsonBadRequest(
+                            null,
+                            getMessage("manga.invalid.id"),
+                            null);
+                        
+                } 
+            }         
+        } catch(err){
+            return res.jsonServerError(
+                null,
+                null,
+                null
+            );
+            
+        }
+                      
     }  
 }
