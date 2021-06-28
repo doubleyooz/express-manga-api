53f44d66a09692f2be3907f976b2ba68456e48dc
WaifuForever <itachiut1r4@gmail.com>
2021-06-28 18:11:46 -0300

    user menu

--- frontend/src/Components/Menu/Horizontal/index.js
+++ frontend/src/Components/Menu/Horizontal/index.js
@@ -1,36 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { useSpring, animated } from 'react-spring'
 
-
+import { Context } from '../../../Contexts/AuthProvider'
 import './styles.css';
 
 export default function Menu(){
-    const [currentNav, setCurrentNav] = useState("");  
-   
+    const [showOptions, setShowOptions] = useState(false);  
+
+    const { token, setToken, handleLogin } = useContext(Context)
+
    
     return (
         <ul className="horizontal-navbar">
             
-                <Link to='/'>
-                    <button className="profile">
-                        <ul className="options">
+            
+                       
 
+            <Link to='/'> 
+                <li className="nav-button">
+
+                </li>         
+            </Link> 
+
+            <Link to='/'>
+                <li className="nav-button">
+
+                </li>         
+            </Link> 
+            <div className="menu">
+                <button className="profile" onClick={() => setShowOptions(!showOptions)}>
+                    
+                </button>
+        
+                    {token?                     
+                        <ul className={showOptions ? "options-display" : "options"}>
+                            <Link to='/user'> 
+                                <li>Profile</li>        
+                            </Link>                        
+                            <li>Config</li>                        
+                            <Link to='/'> 
+                                <li onClick={() => setToken(undefined)}>Logout</li>        
+                            </Link> 
+                            
                         </ul>
-                    </button>         
-                </Link> 
-
-                <Link to='/'> 
-                    <li className="nav-button" style={currentNav === "a" ? {backgroundColor: "#41357E"}  : {backgroundColor: ""}}onClick={() => setCurrentNav('a')}>
-
-                    </li>         
-                </Link> 
-
-                <Link to='/'>
-                    <li className="nav-button">
-
-                    </li>         
-                </Link> 
-               
+                        :
+                        
+                        <ul className={showOptions ? "options-display" : "options"}>
+                            <Link to='/Login'> 
+                                <li>Login</li>        
+                            </Link>                      
+                                            
+                            <Link to='/'> 
+                                <li>Create account</li>        
+                            </Link> 
+                            
+                        </ul>
+                         
+                    }    
+            </div>
+           
+            
             
         </ul>
                
