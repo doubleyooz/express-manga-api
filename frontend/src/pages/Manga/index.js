6fd9de78545bb98ee0dbe803de7dbf9f9044c6dc
WaifuForever <itachiut1r4@gmail.com>
2021-07-02 11:57:29 -0300

    refactor: manga page design

--- frontend/src/pages/Manga/index.js
+++ frontend/src/pages/Manga/index.js
@@ -16,6 +16,7 @@ const Manga = (props) =>{
     
     const [chapters, setChapters] = useState([])    
     const [manga, setManga] = useState({})
+    const [isSelected, setIsSelected] = useState('a');
     const [loading, setLoading] = useState(true)
 
     const {manga_title} = useParams()
@@ -89,46 +90,82 @@ const Manga = (props) =>{
             
     }, [loading]) // <-- empty dependency array
 
-    console.log(manga_title)
-    return <>   
-        <div className="manga-page">
-            <div className="header-board">
-                <img src={process.env.REACT_APP_SERVER + "/" + manga_title + "/" + manga.cover}
-                        alt= {"cover"}                  
-                                         
-                    />
+    function ResponsiveImage( { src, width, height } ) {
 
-                <div className="title">
-                    {manga_title}
+        return (
+            
+            
+            <div className="responsive-image" >
+                <div className="resizer" style={ {
+                    paddingTop: "42%"
+                    } } />
+                <div 
+                    className="image"               
+                    style={{
+                        backgroundImage: `url('${process.env.REACT_APP_SERVER + manga_title + "/" + manga.cover}')`,
+                        backgroundPosition: 'center center',
+                        
+                    }}
+                    alt={"cover"}/>
+                <div className="content" style={ { width: '1439px'} } />
+            </div>
+        );
+    }
+
+    return <>   
+        <div className="manga-board">
+            <div className="card">
+               
+                <ResponsiveImage />
+                
+               
+
+            </div>
+            <div className="content-container">
+                <div className="toogler">
+                    <button onClick={() => setIsSelected("a")}>info</button>
+                    <button onClick={() => setIsSelected("b")}>chapters</button>
+                    <button onClick={() => setIsSelected("c")}>art</button>
                 </div>
 
-                <span>{manga.synopsis}</span>
-
-
-            </div>
-            <div className="list">
-
-                {!loading ? chapters.length !== 0 ? chapters.map((chapter, index) => (       
-                    <div className="chapter">
-                        <Link to={{ pathname: `/Manga/${manga_title.replace(" ", "%20")}/${chapter.number}`, 
-                            state: {chapter: chapter, chapters: manga.chapters}}}>                    
-                            <h2>{chapter.title}</h2>
-                            Chapter {chapter.number}
-                            
-                        
-                        </Link>
-                        <h4>Scan_name</h4>
-                    </div>            
-                   
+                <div className="info" style={isSelected === "a" ? {display: 'block'} : {display: 'none'}}>
                     
-                )): <div>No chapters to display</div>
-                  : <div>Loading...</div>
+                    <div className="title">
+                        {manga_title}
+                    </div>
 
-            }
+                    <span>{manga.synopsis}</span>
+                </div>
 
+                <div className="list" style={isSelected === "b" ? {display: 'block'} : {display: 'none'}}>
+
+                    {!loading ? chapters.length !== 0 ? chapters.map((chapter, index) => (       
+                        <div className="chapter">
+                            <Link to={{ pathname: `/Manga/${manga_title.replace(" ", "%20")}/${chapter.number}`, 
+                                state: {chapter: chapter, chapters: manga.chapters}}}>                    
+                                <h2>{chapter.title}</h2>
+                                Chapter {chapter.number}
+                                
+                            
+                            </Link>
+                            <h4>Scan_name</h4>
+                        </div>            
+                    
+                        
+                    )): <div>No chapters to display</div>
+                    : <div>Loading...</div>
+
+                    }
+
+
+                </div>
+
+                <div className="art" style={isSelected === "c" ? {display: 'block'} : {display: 'none'}}>
+                  Art
+                </div>
 
             </div>
-
+           
         </div>
       
         
