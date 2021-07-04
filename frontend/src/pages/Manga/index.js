a5581a841249d3e9bedf0f06a0901a6718a5befa
WaifuForever <itachiut1r4@gmail.com>
2021-07-04 15:31:37 -0300

    fix - manga page switch sections

--- frontend/src/pages/Manga/index.js
+++ frontend/src/pages/Manga/index.js
@@ -96,8 +96,7 @@ const Manga = (props) =>{
 
     function ResponsiveImage({width, height}) {
 
-        return (
-            
+        return (            
             
             <div className="responsive-image" >
                 <div className="resizer" style={ {
@@ -120,20 +119,45 @@ const Manga = (props) =>{
         console.log(manga)
         switch(param) {
             case 0:
-                return <><div>On going</div></>;
+                return <>
+                    <div class="sign" style={{backgroundColor: 'green'}}></div>
+                    <div>On going</div>
+                </>;
 
             case 1:
-                return <><div>Complete</div></>;
+                return <>
+                     <div class="sign" style={{backgroundColor: 'blue'}}></div>
+                    <div>Complete</div>
+                </>;
 
             case 2:
-                return <><div>Dropped</div></>;
+                return <>
+                    <div class="sign" style={{backgroundColor: 'red'}}></div>
+                    <div>Dropped</div>
+                </>;
           
             default:
-                return <><div>Unknow</div></>;;
+                return <>
+                    <div class="sign" style={{backgroundColor: 'gray'}}></div>
+                    <div>Unknow</div>
+                </>;;
         }
     }
       
+    const styles = {
+        display : {
+            transform: 'scaleX(1)',
+            transformOrigin: 'right',
+            transition: '.25s'
+        },
+        no_display : {
+            transform: 'scaleX(0)',
+            transformOrigin: 'right',
+            transition: '.25s'
+        }
 
+    }
+   
     return <>   
         <div className="manga-board">
             <div className="card">
@@ -144,51 +168,70 @@ const Manga = (props) =>{
 
             </div>
             <div className="content-container">
-                <div className="basic">
+                <div className="header">
                     <div className="title">
                         {manga_title}
                     </div>
-                    {renderStatus(manga.status)}
+                    <div className="status">
+                        {renderStatus(manga.status)}
+                    </div>
+                   
                 </div>
 
                 <div className="toogler">
-                    <button onClick={() => setIsSelected("a")}>info</button>
-                    <button onClick={() => setIsSelected("b")}>chapters</button>
-                    <button onClick={() => setIsSelected("c")}>art</button>
+                    <span 
+                        onClick={() => setIsSelected("a")} 
+                        style={isSelected === "a" ? {backgroundColor: "#000000", color: "#ffffff"} : {backgroundColor: "#999999", color: "#000000"}}>
+                            info
+                    </span>
+                    <span 
+                        onClick={() => setIsSelected("b")}
+                        style={isSelected === "b" ? {backgroundColor: "#000000", color: "#ffffff"} : {backgroundColor: "#999999", color: "#000000"}}>
+                        chapters
+                    </span>
+                    <span 
+                        onClick={() => setIsSelected("c")}
+                        style={isSelected === "c" ? {backgroundColor: "#000000", color: "#ffffff"} : {backgroundColor: "#999999", color: "#000000"}}>
+                            art
+                    </span>
                 </div>
 
-                <div className="info" style={isSelected === "a" ? {display: 'block'} : {display: 'none'}}>
-                    
-                  
+                <div className="container">
 
-                    <span>{manga.synopsis}</span>
-                </div>
-
-                <div className="list" style={isSelected === "b" ? {display: 'block'} : {display: 'none'}}>
-
-                    {!loading ? chapters.length !== 0 ? chapters.map((chapter, index) => (       
-                        <div className="chapter">
-                            <Link to={{ pathname: `/Manga/${manga_title.replace(" ", "%20")}/${chapter.number}`, 
-                                state: {chapter: chapter, chapters: manga.chapters}}}>                    
-                                <h2>{chapter.title}</h2>
-                                Chapter {chapter.number}
-                                
-                            
-                            </Link>
-                            <h4>Scan_name</h4>
-                        </div>            
-                    
+                    <div className="info" style={isSelected === "a" ? {...styles.display}: {...styles.no_display}}>
                         
-                    )): <div>No chapters to display</div>
-                    : <div>Loading...</div>
+                    
 
-                    }
+                        <span>{manga.synopsis}</span>
+                    </div>
+
+                    <div className="list" style={isSelected === "b" ? {...styles.display} : {...styles.no_display}}>
+
+                        {!loading ? chapters.length !== 0 ? chapters.map((chapter, index) => (       
+                            <div className="chapter">
+                                <Link to={{ pathname: `/Manga/${manga_title.replace(" ", "%20")}/${chapter.number}`, 
+                                    state: {chapter: chapter, chapters: manga.chapters}}}>                    
+                                    <h2>{chapter.title}</h2>
+                                    Chapter {chapter.number}
+                                    
+                                
+                                </Link>
+                                <h4>Scan_name</h4>
+                            </div>            
+                        
+                            
+                        )): <div>No chapters to display</div>
+                        : <div>Loading...</div>
+
+                        }
 
 
-                </div>
+                    </div>
 
-                <div className="art" style={isSelected === "c" ? {display: 'block'} : {display: 'none'}}>
-                  Art
+                    <div className="art" style={isSelected === "c" ? {...styles.display} : {...styles.no_display}}>
+                        Art
+                    </div>
+
                 </div>
 
             </div>
