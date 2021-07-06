6cb2fc010d5d0befb4a9392e5ec70da3980eb982
WaifuForever <itachiut1r4@gmail.com>
2021-07-06 15:20:17 -0300

    update: chapter component

--- frontend/src/Components/Chapter/index.js
+++ frontend/src/Components/Chapter/index.js
@@ -9,8 +9,16 @@ export default function Chapter(props){
 
     const { token, setToken, handleLogin } = useContext(Context)
     let history = useHistory();
-   
-    console.log(props)
+    
+    console.log(props.data)
+
+    function route(){
+        history.push(
+            `/Manga/${props.data.manga.title.replace(" ", "%20")}/${props.data.chapter.number}`, 
+            {chapter:  props.data.chapter, chapters: props.data.manga.chapters}
+        )
+    }
+
     return (
       
        
@@ -18,15 +26,20 @@ export default function Chapter(props){
            
             <div className="row">
               
-             
-                <Link to={{ pathname: `/Manga/${props.data.manga.title.replace(" ", "%20")}/${props.data.number}`, 
-                                    state: {chapter:  props.data.chapter, chapters: props.data.manga.chapters}}}> 
-                    <div className="title">{props.data.title}</div>
-                    <div className="number">Chapter {props.data.number}</div>                   
-                                  
-                                
-                </Link>
-                 <h4>Scan_name</h4>
+              
+                <div className="box-1" onClick={() => route()}>
+                   
+                    <div className="number">{`Chapter ${props.data.chapter.number} : `}</div>
+                    <div className="title">{props.data.chapter.title}</div>
+                    <div className="date">{`${props.data.chapter.updatedAt}`}</div> 
+                </div>
+                                          
+
+                      
+                <div className="box-2">
+                    <h4>Scan_name</h4>
+                </div>
+                
             </div>
            
                
