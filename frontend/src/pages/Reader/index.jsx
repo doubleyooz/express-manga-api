3de616f59716bc0474a081bf8d8e2a4a1506881f
WaifuForever <itachiut1r4@gmail.com>
2021-07-18 10:10:39 -0300

    add: reader footer

--- frontend/src/pages/Reader/index.jsx
+++ frontend/src/pages/Reader/index.jsx
@@ -253,7 +253,30 @@ const Reader = (props) =>{
             }    
               
             </div>
-          
+            <div className="footer-reader" style={!vertical ? {}  : {display: "none"}}>
+                
+               
+                    <button className='comments' onClick= {() => {}}>comments</button>
+                    <div className="rate">
+                        <input type="radio" id="star5" name="rate" value="5" />
+                        <label for="star5" title="text">★</label>
+                        <input type="radio" id="star4" name="rate" value="4" />
+                        <label for="star4" title="text">★</label>
+                        <input type="radio" id="star3" name="rate" value="3" />
+                        <label for="star3" title="text">★</label>
+                        <input type="radio" id="star2" name="rate" value="2" />
+                        <label for="star2" title="text">★</label>
+                        <input type="radio" id="star1" name="rate" value="1" />
+                        <label for="star1" title="text">★</label>
+                    </div>
+                    <button className='report' onClick={() => {}}>!!</button>
+                    <div className="zoom">zoom</div>
+                    
+                
+                
+            
+       
+            </div>
           
         </div>
       
