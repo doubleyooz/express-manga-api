d4ed90c6f7f9c43ff086bb36669de140b3e5706f
WaifuForever <itachiut1r4@gmail.com>
2021-07-29 11:55:15 -0300

    remove: easyAuth.js

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -8,7 +8,7 @@ import { getMessage } from '../common/messages.js';
 
 dotenv.config()
 
-export function auth(roles = []){
+function auth(roles = []){
     return async (req, res, next) => {
         try{          
             const [, token] = req.headers.authorization.split(" ")
@@ -72,4 +72,96 @@ export function auth(roles = []){
             
         }        
     }
+}
+
+function easyAuth(){
+    return async (req, res, next) => {
+        try{  
+                  
+            if(!req.headers.authorization){
+                next()
+            } else{
+
+                const [, token] = req.headers.authorization.split(" ")             
+           
+                if(!token){
+                    next()
+                } else {
+                     
+                    let payload = null
+                
+                    payload = jwt.verifyJwt(token, 1)      
+                                
+                    User.exists({_id: payload.id, active: true, token_version: payload.token_version}).then(result => {
+                        if (result){
+                            try{                                
+                                var current_time = Date.now().valueOf() / 1000;
+                                console.log(payload)
+                                console.log(payload.exp - payload.iat)
+                                console.log(payload.exp - current_time)
+                                console.log(current_time)
+                                if ((payload.exp - payload.iat)/2 > payload.exp - current_time) {
+                                    let new_token = jwt.generateJwt({id: payload.id, role: payload.role, token_version: payload.token_version}, 1)
+                                    req.new_token = `Bearer ${new_token}`
+                                    console.log(`New Token: ${new_token}`)
+                                } else{
+                                    console.log("Token not expired")
+                                }
+                                let dict = {
+                                    "": 0,
+                                    "Scan": 1,
+                                    "User": 2
+                                }
+                                
+                                req.role =  CryptoJs.AES.encrypt(dict[payload.role].toString(), `${process.env.SHUFFLE_SECRET}`)
+                                payload = null
+                                dict = null
+                                next();
+                                
+                            }   catch(err){
+                                    console.log("something went wrong.")
+                                    console.log(err)
+                                    next()
+                                
+                            }           
+                            
+                        } else{ 
+                            next()
+                            
+                        }
+                    }).catch(err =>{
+                        next()
+                        
+                    })
+                }
+               
+                
+            }
+            
+        
+                
+        } catch(err){            
+            next()
+            
+        }
+       
+               
+     
+            
+        
+        
+        
+    }
+   
+    
+
+    
+
+    
+
+}
+
+export {
+    auth,
+    easyAuth
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,8 +15,8 @@ import ChapterMiddleware from './middlewares/Chapter.js';
 import MangaMiddleware from './middlewares/Manga.js';
 import UserMiddleware from './middlewares/User.js';
 
-import { auth as Authorize }  from './middlewares/auth.js';
-import { getRole } from './middlewares/easyAuth.js';
+import { auth as Authorize, easyAuth }  from './middlewares/auth.js';
+
 import UploadMiddleware from './middlewares/upload.js';
 
 
@@ -40,8 +40,8 @@ routes.post("/google-sign-in", SessionController.google_sign_in)
 routes.post("/google-sign-up", UserMiddleware.valid_google_sign_up, UserController.store)
 routes.get('/refresh-token', SessionController.refreshAccessToken)
 
-routes.get('/user/read', getRole(), UserMiddleware.valid_user_read, UserController.read);
-routes.get('/user/list', getRole(),  UserMiddleware.valid_user_list,UserController.list);
+routes.get('/user/read', easyAuth(), UserMiddleware.valid_user_read, UserController.read);
+routes.get('/user/list', easyAuth(),  UserMiddleware.valid_user_list,UserController.list);
 routes.put('/user/update', Authorize(["Scan", "User"]), UserMiddleware.valid_user_update, UserController.update)
 routes.delete('/user/delete', Authorize(["Scan", "User"]), UserMiddleware.valid_user_remove, UserController.remove);
 routes.put('/user/like', Authorize("User"), LikeController.likeUser);
@@ -49,16 +49,16 @@ routes.get('/user/notify', Authorize("Scan"), NotifyController.notifyUsers)
 
 
 routes.post('/manga/post', Authorize("Scan"), UploadMiddleware.upload_single, MangaMiddleware.valid_manga_store, MangaController.store);
-routes.get('/manga/list',  getRole(), MangaMiddleware.valid_manga_list, MangaController.list);
-routes.get('/manga/read',  getRole(), MangaMiddleware.valid_manga_read, MangaController.read);
+routes.get('/manga/list',  easyAuth(), MangaMiddleware.valid_manga_list, MangaController.list);
+routes.get('/manga/read',  easyAuth(), MangaMiddleware.valid_manga_read, MangaController.read);
 routes.put('/manga/update', Authorize("Scan"), MangaMiddleware.valid_manga_update, MangaController.update);
 routes.delete('/manga/delete', Authorize("Scan"), MangaMiddleware.valid_manga_remove, MangaController.remove);
 routes.put('/manga/like', Authorize("User"), LikeController.likeManga);
 routes.put('/manga/pin', Authorize("User"), LikeController.pinManga);
 
 routes.post('/chapter/post', Authorize("Scan"), UploadMiddleware.upload_many_manga, ChapterMiddleware.valid_chapter_store, ChapterController.store);
-routes.get('/chapter/list', getRole(), ChapterMiddleware.valid_chapter_list, ChapterController.list);
-routes.get('/chapter/read', getRole(), ChapterMiddleware.valid_chapter_read, ChapterController.read);
+routes.get('/chapter/list', easyAuth(), ChapterMiddleware.valid_chapter_list, ChapterController.list);
+routes.get('/chapter/read', easyAuth(), ChapterMiddleware.valid_chapter_read, ChapterController.read);
 routes.put('/chapter/update', Authorize("Scan"), ChapterController.update);
 routes.delete('/chapter/delete', Authorize("Scan"), ChapterMiddleware.valid_chapter_remove,  ChapterController.remove);
 
