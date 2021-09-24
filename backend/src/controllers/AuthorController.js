1abb02d42ff134bc05518dc51ed0df209526b57e
WaifuForever <itachiut1r4@gmail.com>
2021-09-24 20:42:12 -0300

    fix: overwriting author

--- backend/src/controllers/AuthorController.js
+++ backend/src/controllers/AuthorController.js
@@ -1,265 +1,282 @@
-import fs from 'fs';
-import { parseISO } from 'date-fns';
+import fs from "fs";
+import { parseISO } from "date-fns";
 
-import Author from '../models/Author.js';
-import Manga from '../models/Manga.js';
+import Author from "../models/Author.js";
+import Manga from "../models/Manga.js";
 
-import { getMessage } from '../common/messages.js';
+import { getMessage } from "../common/messages.js";
 
-async function store(req, res){         
-    const { type, name, birthDate, socialMedia, deathDate, biography } = req.body;
-    const new_token = (req.new_token) ? req.new_token : null;
-    req.new_token = null
-   
-    const storedAuthor = await Author.findOne({name: name})
+async function store(req, res) {
+	const { type, name, birthDate, socialMedia, deathDate, biography } = req.body;
+	const new_token = req.new_token ? req.new_token : null;
+	req.new_token = null;
 
-    if(storedAuthor !== null){            
-        if(storedAuthor.type.includes(type)){                
-            Object.keys(req.files).forEach((i) => {
-                let file = req.files[i];   
-                fs.unlinkSync('uploads/' + "authors/" + type + "/" + name + "/" + file.filename)    
-               
-            });
-                          
-            return res.jsonBadRequest(null, getMessage("author.error.duplicate"), new_token)
-        } else {
-                           
-            storedAuthor.type.push(type)
-            storedAuthor.save().then(result=>{
-                Object.keys(req.files).forEach((i) => {
-                    let file = req.files[i];   
-                    fs.unlinkSync('uploads/' + "authors/" + type + "/" + name + "/" + file.filename)    
-                   
-                });
-                return res.jsonOK(result, getMessage("author.save.success"), new_token)
-    
-            }).catch(err=>{
-                console.log(err)
-                Object.keys(req.files).forEach((i) => {
-                    let file = req.files[i];   
-                    fs.unlinkSync('uploads/' + "authors/" + storedAuthor.type + "/" + name + "/" + file.filename)    
-                
-                });
-                return res.jsonServerError(null, null, err)
-            })
-        }
-    } else {
- 
-        req.auth = null
+	const storedAuthor = await Author.findOne({ name: name });
 
-        let jsonString = [];         
-                                            
-        Object.keys(req.files).forEach((i) => {
-            let file = req.files[i];
-            
-            let temp = { 
-                originalname:  file.originalname,
-                size: file.size,
-                filename: file.filename,
-                        
-            }                            
-        
-            jsonString.push(JSON.parse(JSON.stringify(temp)));
-            
-        });
+	if (storedAuthor !== null) {
+		if (storedAuthor.type.includes(type)) {
+			Object.keys(req.files).forEach((i) => {
+				let file = req.files[i];
+				fs.unlinkSync(
+					"uploads/" + "authors/" + type + "/" + name + "/" + file.filename
+				);
+			});
 
-        const author = new Author({
-            photos: [],
-            type: [type],
-            name: name,
-            birthDate: parseISO(birthDate),
-            deathDate: deathDate ? parseISO(deathDate) : null,
-            socialMedia: socialMedia,    
-            biography: biography              
-           
-            //comments?
+			return res.jsonBadRequest(
+				null,
+				getMessage("author.error.duplicate"),
+				new_token
+			);
+		} else {
+			storedAuthor.type.push(type);
+			storedAuthor
+				.save()
+				.then((result) => {
+					Object.keys(req.files).forEach((i) => {
+						let file = req.files[i];
+						fs.unlinkSync(
+							"uploads/" + "authors/" + type + "/" + name + "/" + file.filename
+						);
+					});
+					return res.jsonOK(
+						result,
+						getMessage("author.save.success"),
+						new_token
+					);
+				})
+				.catch((err) => {
+					console.log(err);
+					Object.keys(req.files).forEach((i) => {
+						let file = req.files[i];
+						fs.unlinkSync(
+							"uploads/" +
+								"authors/" +
+								storedAuthor.type +
+								"/" +
+								name +
+								"/" +
+								file.filename
+						);
+					});
+					return res.jsonServerError(null, null, err);
+				});
+		}
+	} else {
+		req.auth = null;
 
-        });
-        author.photos = jsonString
-                
-        author.save().then(result => {   
-        
-            return res.jsonOK(result, getMessage("author.save.success"), new_token)
-        
-                                                    
-        }).catch(err => {
-            console.log(err)
-            Object.keys(req.files).forEach((i) => {
-                let file = req.files[i];   
-                fs.unlinkSync('uploads/' + "authors/" + type + "/" + name + "/" + file.filename)    
-               
-            });
-            return res.jsonServerError(null, null, err)
-        
-        });
-    }
+		let jsonString = [];
 
-       
-           
+		Object.keys(req.files).forEach((i) => {
+			let file = req.files[i];
+
+			let temp = {
+				originalname: file.originalname,
+				size: file.size,
+				filename: file.filename,
+			};
+
+			jsonString.push(JSON.parse(JSON.stringify(temp)));
+		});
+
+		const author = new Author({
+			photos: [],
+			type: [type],
+			name: name,
+			birthDate: parseISO(birthDate),
+			deathDate: deathDate ? parseISO(deathDate) : null,
+			socialMedia: socialMedia,
+			biography: biography,
+
+			//comments?
+		});
+		author.photos = jsonString;
+
+		author
+			.save()
+			.then((result) => {
+				return res.jsonOK(result, getMessage("author.save.success"), new_token);
+			})
+			.catch((err) => {
+				console.log(err);
+				Object.keys(req.files).forEach((i) => {
+					let file = req.files[i];
+					fs.unlinkSync(
+						"uploads/" + "authors/" + type + "/" + name + "/" + file.filename
+					);
+				});
+				return res.jsonServerError(null, null, err);
+			});
+	}
 }
 
-async function read(req, res){
-    const { author_id } = req.query;
-    const new_token = (req.new_token) ? req.new_token : null;       
-    req.new_token = null
+async function read(req, res) {
+	const { author_id } = req.query;
+	const new_token = req.new_token ? req.new_token : null;
+	req.new_token = null;
 
-    if (author_id){        
-       const author = await Author.findById(author_id).exec();
+	if (author_id) {
+		const author = await Author.findById(author_id).exec();
 
-       if(author === null){
-           return res.jsonNotFound(author, getMessage("author.notfound", new_token))
-       }
-       return res.jsonOK(author, getMessage("author.read.success"), new_token)
-    }      
-
-    else{            
-        return res.jsonBadRequest(null,null, null)
-    }
-                    
-
+		if (author === null) {
+			return res.jsonNotFound(author, getMessage("author.notfound", new_token));
+		}
+		return res.jsonOK(author, getMessage("author.read.success"), new_token);
+	} else {
+		return res.jsonBadRequest(null, null, null);
+	}
 }
 
-async function list(req, res){
-    const { type, name } = req.query;
-    const new_token = (req.new_token) ? req.new_token : null;       
-    req.new_token = null
+async function list(req, res) {
+	const { type, name } = req.query;
+	const new_token = req.new_token ? req.new_token : null;
+	req.new_token = null;
 
-   
+	let docs = [];
 
-    let docs = [];
+	if (type) {
+		if (name) {
+			(
+				await Author.find({
+					name: { $regex: name, $options: "i" },
+					type: type,
+				}).sort("updatedAt")
+			).forEach(function (doc) {
+				docs.push(doc);
+			});
+		} else {
+			(await Author.find({ type: type }).sort("updatedAt")).forEach(function (
+				doc
+			) {
+				docs.push(doc);
+			});
+		}
+	} else if (name) {
+		(
+			await Author.find({ name: { $regex: name, $options: "i" } }).sort(
+				"updatedAt"
+			)
+		).forEach(function (doc) {
+			docs.push(doc);
+		});
+	} else {
+		(await Author.find().sort("updatedAt")).forEach(function (doc) {
+			docs.push(doc);
+		});
+	}
 
-    if (type){
-        if(name){
-            (await Author.find({name: {$regex: name, $options: "i"}, type: type}).sort('updatedAt')).forEach(function (doc){
-                docs.push(doc)
-            });
-        } else{
-            (await Author.find({type: type}).sort('updatedAt')).forEach(function (doc){
-                docs.push(doc)
-            });
-        }
-       
-    } else if (name){
-        (await Author.find({name: {$regex: name, $options: "i"} }).sort('updatedAt')).forEach(function (doc){
-            docs.push(doc)
-        });
-    } else{
-        (await Author.find().sort('updatedAt')).forEach(function (doc){
-            docs.push(doc)
-        });
-    }  
-            
-    
-           
-    if (docs.length === 0){
-        return res.jsonNotFound(docs, getMessage("author.list.empty"), new_token)
-    } else{
-       
-        return res.jsonOK(docs, getMessage("author.list.success") + ": " + docs.length, new_token)
-    }
-                    
+	if (docs.length === 0) {
+		return res.jsonNotFound(docs, getMessage("author.list.empty"), new_token);
+	} else {
+		return res.jsonOK(
+			docs,
+			getMessage("author.list.success") + ": " + docs.length,
+			new_token
+		);
+	}
 }
 
-async function update(req, res){   
-    const new_token = (req.new_token) ? req.new_token : null;
-    req.new_token = null
-    req.body.updatedAt = Date.now()
+async function update(req, res) {
+	const new_token = req.new_token ? req.new_token : null;
+	req.new_token = null;
+	req.body.updatedAt = Date.now();
 
-  
-    Author.findByIdAndUpdate(req.body.author_id, req.body).select({type: 1, name: 1}).then(doc => {
-        if(!doc){
-            return res.jsonNotFound(null, getMessage("author.notfound"), new_token)
-        }
-       
-        else{   
-            let temp;
-            req.body.type ? temp = req.body.type : temp = doc.type        
-            if(req.body.name){                           
-               
-                const currPath = `./uploads/authors/${doc.type}/${doc.name}`
-                const newPath = `./uploads/authors/${temp}/${req.body.name}`
-                
-                fs.rename(currPath, newPath, function(err) {
-                    if (err) {
-                    console.log(err)
-                    } else {
-                    console.log("Successfully renamed the directory.")
-                    }
-                })
-            }
-        
-            
-            return res.jsonOK(doc, getMessage("author.update.success"), new_token)
-        }
-    }).catch(err => {       
-        return res.jsonServerError(err, null, new_token)
-    })
-             
-      
+	if (await Author.exists({ name: req.body.name }))
+		return res.jsonBadRequest(
+			null,
+			getMessage("author.error.overwrite"),
+			new_token
+		);
+
+	Author.findByIdAndUpdate(req.body.author_id, req.body)
+		.select({ type: 1, name: 1 })
+		.then((doc) => {
+			if (!doc) {
+				return res.jsonNotFound(null, getMessage("author.notfound"), new_token);
+			} else {
+				let temp = req.body.type ? req.body.type : doc.type;
+
+				if (req.body.name) {
+					const currPath = `./uploads/authors/${doc.type}/${doc.name}`;
+					const newPath = `./uploads/authors/${temp}/${req.body.name}`;
+
+					fs.rename(currPath, newPath, function (err) {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log("Successfully renamed the directory.");
+						}
+					});
+				}
+
+				return res.jsonOK(doc, getMessage("author.update.success"), new_token);
+			}
+		})
+		.catch((err) => {
+			return res.jsonServerError(err, null, new_token);
+		});
 }
 
+async function remove(req, res) {
+	const { author_id } = req.query;
+	const author = await Author.findById(author_id);
 
-async function remove(req, res){
-    const { author_id } = req.query;            
-    const author = await Author.findById(author_id);
+	const new_token = req.new_token ? req.new_token : null;
+	req.new_token = null;
 
-    const new_token = (req.new_token) ? req.new_token : null;
-    req.new_token = null
-    
-  
+	if (author) {
+		Author.deleteOne({ _id: author_id })
+			.then((answer) => {
+				let mangas = [];
+				let dir =
+					"uploads/" + "authors/" + author.type + "/" + author.name + "/";
 
-    if(author){                             
-     
-        Author.deleteOne({ _id: author_id }).then(answer => {
-            let mangas = []
-            let dir = 'uploads/' + "authors/" + author.type + "/" + author.name + "/" 
-            
-            author.photos.forEach((file) => {                               
-                fs.unlinkSync(dir + file.filename)    
-            
-            });
+				author.photos.forEach((file) => {
+					fs.unlinkSync(dir + file.filename);
+				});
 
-            fs.rmdir(dir, { recursive: true }, (err) => {
-                if(err){
-                    console.log(err)
-                }
-                                
-                
-            });
+				fs.rmdir(dir, { recursive: true }, (err) => {
+					if (err) {
+						console.log(err);
+					}
+				});
 
-            fs.rmdirSync(dir, {recursive: true})
+				fs.rmdirSync(dir, { recursive: true });
 
-            author.works.forEach(manga_id => {
-                Manga.findById(manga_id).then(manga => {
-                    manga[author.type] =  manga[author.type].filter(function (crt_id){
-                    
-                        return crt_id.toString() !== author_id.toString()
-                          
-                    });
-                    manga.save().then(answer => {  
-                        
-
-                        mangas.push({'author-deleted': manga_id})
-                        
-                    }).catch(err => {
-                        mangas.push({'author-not-deleted': manga_id})
-                    })
-
-                }).catch(err =>{
-                    mangas.push({'manga-not-found': manga_id})
-                })
-            })
-            return res.jsonOK({ removed: true, mangas: mangas }, getMessage("author.delete.success"), new_token)
-
-        }).catch(err=>{
-            return res.jsonBadRequest(null, getMessage("author.notfound"), new_token)
-        })
-                     
-        
-    } else {
-        return res.jsonServerError(null, null, new_token)
-    }
-
+				author.works.forEach((manga_id) => {
+					Manga.findById(manga_id)
+						.then((manga) => {
+							manga[author.type] = manga[author.type].filter(function (crt_id) {
+								return crt_id.toString() !== author_id.toString();
+							});
+							manga
+								.save()
+								.then((answer) => {
+									mangas.push({ "author-deleted": manga_id });
+								})
+								.catch((err) => {
+									mangas.push({ "author-not-deleted": manga_id });
+								});
+						})
+						.catch((err) => {
+							mangas.push({ "manga-not-found": manga_id });
+						});
+				});
+				return res.jsonOK(
+					{ removed: true, mangas: mangas },
+					getMessage("author.delete.success"),
+					new_token
+				);
+			})
+			.catch((err) => {
+				return res.jsonBadRequest(
+					null,
+					getMessage("author.notfound"),
+					new_token
+				);
+			});
+	} else {
+		return res.jsonServerError(null, null, new_token);
+	}
 }
 
-export default {store, read, list, update, remove}
+export default { store, read, list, update, remove };
--- backend/src/middlewares/author.js
+++ backend/src/middlewares/author.js
@@ -1,202 +1,201 @@
-import yup from 'yup';
-import mongoose from 'mongoose';
-import { differenceInCalendarDays } from 'date-fns'
+import yup from "yup";
+import mongoose from "mongoose";
+import { differenceInCalendarDays } from "date-fns";
 
-import { getMessage } from '../common/messages.js';
+import { getMessage } from "../common/messages.js";
 
-
-function isValidMongoId(object_id){
-    try{
-        if (mongoose.Types.ObjectId.isValid(object_id)) {   
-            if (String(new mongoose.Types.ObjectId(object_id)) === object_id){
-                return true;
-            } else{
-                return false;
-            }
-        } else{
-            return false;
-        }
-    }catch(err){
-        console.log(err)
-        return false;
-    }
-
+function isValidMongoId(object_id) {
+	try {
+		if (mongoose.Types.ObjectId.isValid(object_id)) {
+			if (String(new mongoose.Types.ObjectId(object_id)) === object_id) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
 }
 
+async function valid_author_store(req, res, next) {
+	let currentDate = new Date();
+	currentDate.setFullYear(currentDate.getFullYear() - 10);
 
-async function valid_author_store(req, res, next){         
-    let currentDate = new Date()
-    currentDate.setFullYear( currentDate.getFullYear() - 10 );
-       
-    const schema = yup.object().shape({
-        type: yup.string("type must be a string.").strict().matches(/(writer|artist)/, null).required(),
-        name: yup.string("name must be a string.").strict().required(),
-        birthDate: yup.date().max(currentDate).required(),
-        deathDate: yup.date().min(
-            yup.ref('birthDate') + 3650,
-            "death date must be at least 10 years longer than birthDate"
-        ),
-        socialMedia: yup.string("socialMedia must be a string.").strict().required(),        
-        biography: yup.string("biography").strict().min(15, getMessage("author.invalid.biography.short")).required()
-        
-    })
+	const schema = yup.object().shape({
+		type: yup
+			.string("type must be a string.")
+			.strict()
+			.matches(/(writer|artist)/, null)
+			.required(),
+		name: yup.string("name must be a string.").strict().required(),
+		birthDate: yup.date().max(currentDate).required(),
+		deathDate: yup
+			.date()
+			.min(
+				yup.ref("birthDate") + 3650,
+				"death date must be at least 10 years longer than birthDate"
+			),
+		socialMedia: yup
+			.string("socialMedia must be a string.")
+			.strict()
+			.required(),
+		biography: yup
+			.string("biography")
+			.strict()
+			.min(15, getMessage("author.invalid.biography.short"))
+			.required(),
+	});
 
-   
-    schema.validate(req.body).then(() => {      
-       next();
-    })
-    .catch(function(e) {
-        console.log(e)
-        return res.jsonBadRequest(null, null, e)
-        
-    });
-                                    
+	schema
+		.validate(req.body)
+		.then(() => {
+			next();
+		})
+		.catch(function (e) {
+			console.log(e);
+			return res.jsonBadRequest(null, null, e);
+		});
 }
 
-async function valid_author_read(req, res, next){
-    const {manga_id, title} = req.query
+async function valid_author_read(req, res, next) {
+	const { manga_id, title } = req.query;
 
-    let schema = yup.object().shape({
-        title: yup.string("title must be a string.").when(["manga_id"], {
-            is: (manga_id) => !manga_id,
-            then: yup.string().required()
-        }),
-        manga_id: yup.string("manga_id must be a string.").when(["title"], {
-            is: (title) => !title,
-            then: yup.string().required()
-        }),
+	let schema = yup.object().shape({
+		title: yup.string("title must be a string.").when(["manga_id"], {
+			is: (manga_id) => !manga_id,
+			then: yup.string().required(),
+		}),
+		manga_id: yup.string("manga_id must be a string.").when(["title"], {
+			is: (title) => !title,
+			then: yup.string().required(),
+		}),
+	});
 
-    })
-           
-
-    try{
-        schema.validate(req.query).then(() => {
-            if(manga_id){
-                if(isValidMongoId(manga_id)){
-                    next();  
-                } else{
-                    return res.jsonBadRequest(
-                        null,
-                        getMessage("manga.invalid.id"),
-                        null);
-                }
-               
-            } else {
-                
-                next();
-                
-            }
-        
-        })
-        .catch((err) => {
-            return res.jsonBadRequest(null, null, err.errors)              
-           
-       })
-       
-
-    } catch(err){
-        return res.jsonBadRequest(null, null, err.errors)
-    }
-   
+	try {
+		schema
+			.validate(req.query)
+			.then(() => {
+				if (manga_id) {
+					if (isValidMongoId(manga_id)) {
+						next();
+					} else {
+						return res.jsonBadRequest(
+							null,
+							getMessage("manga.invalid.id"),
+							null
+						);
+					}
+				} else {
+					next();
+				}
+			})
+			.catch((err) => {
+				return res.jsonBadRequest(null, null, err.errors);
+			});
+	} catch (err) {
+		return res.jsonBadRequest(null, null, err.errors);
+	}
 }
 
-async function valid_author_list(req, res, next){        
-    let schema = yup.object().shape({
-        title: yup.string("title must be a string.").strict(),
-        genre: yup.string("genre must be a string.").strict(),
-        scan: yup.string("scan must be a string.").strict(),
-        recent: yup.boolean()
+async function valid_author_list(req, res, next) {
+	let schema = yup.object().shape({
+		title: yup.string("title must be a string.").strict(),
+		genre: yup.string("genre must be a string.").strict(),
+		scan: yup.string("scan must be a string.").strict(),
+		recent: yup.boolean(),
+	});
 
-    })
-           
-
-    try{
-        schema.validate(req.query).then(() => {                                             
-            next();    
-                    
-        })
-        .catch((err) => {
-            return res.jsonBadRequest(null, null, err.errors)              
-           
-       })
-       
-
-    } catch(err){
-        return res.jsonBadRequest(null, null, err.errors)
-    }
-   
+	try {
+		schema
+			.validate(req.query)
+			.then(() => {
+				next();
+			})
+			.catch((err) => {
+				return res.jsonBadRequest(null, null, err.errors);
+			});
+	} catch (err) {
+		return res.jsonBadRequest(null, null, err.errors);
+	}
 }
 
-async function valid_author_update(req, res, next){        
-    let currentDate = new Date()
-    currentDate.setFullYear( currentDate.getFullYear() - 10 );
-       
-    const schema = yup.object().shape({
-        author_id: yup.string("author_id must be a string.").strict(),
-        type: yup.string("type must be a string.").strict().matches(/(writer|artist)/, null),
-        name: yup.string("name must be a string.").strict(),
-        birthDate: yup.date().max(currentDate),
-        deathDate: yup.date().min(
-            yup.ref('birthDate') + 3650,
-            "death date must be at least 10 years longer than birthDate"
-        ),
-        socialMedia: yup.string("socialMedia must be a string.").strict(),        
-        biography: yup.string("biography").strict().min(15, getMessage("author.invalid.biography.short"))
-        
-    }).test('at-least-one-field', "you must provide at least one field", value =>
-    !!(value.type || value.name || value.birthDate || value.deathDate || value.socialMedia || value.biography)
-  )
-    try{
-        schema.validate(req.body).then(() => {             
-            if(isValidMongoId(req.body.author_id)){
-                schema.cast(req.body, { stripUnknown: true})               
-                next();                                             
-            }
-            else{
-                return res.jsonBadRequest(
-                    null,
-                    getMessage("manga.invalid.id"),
-                    null);
-            }                                
-        })
-        .catch((err) => {
-            return res.jsonBadRequest(null, null, err.errors)              
-           
-       })
-       
+async function valid_author_update(req, res, next) {
+	let currentDate = new Date();
+	currentDate.setFullYear(currentDate.getFullYear() - 10);
 
-    } catch(err){
-        return res.jsonBadRequest(null, null, err.errors)
-    }
-   
+	const schema = yup
+		.object()
+		.shape({
+			author_id: yup.string("author_id must be a string.").strict(),
+			type: yup
+				.string("type must be a string.")
+				.strict()
+				.matches(/(writer|artist)/, null),
+			name: yup.string("name must be a string.").strict(),
+			birthDate: yup.date().max(currentDate),
+			deathDate: yup
+				.date()
+				.min(
+					yup.ref("birthDate") + 3650,
+					"death date must be at least 10 years longer than birthDate"
+				),
+			socialMedia: yup.string("socialMedia must be a string.").strict(),
+			biography: yup
+				.string("biography")
+				.strict()
+				.min(15, getMessage("author.invalid.biography.short")),
+		})
+		.test(
+			"at-least-one-field",
+			"you must provide at least one field",
+			(value) =>
+				!!(
+					value.type ||
+					value.name ||
+					value.birthDate ||
+					value.deathDate ||
+					value.socialMedia ||
+					value.biography
+				)
+		)
+      
+	try {
+		schema
+			.validate(req.body)
+			.then(() => {
+				if (isValidMongoId(req.body.author_id)) {
+					schema.cast(req.body, { stripUnknown: true });
+					next();
+				} else {
+					return res.jsonBadRequest(null, getMessage("manga.invalid.id"), null);
+				}
+			})
+			.catch((err) => {
+				return res.jsonBadRequest(null, null, err.errors);
+			});
+	} catch (err) {
+		return res.jsonBadRequest(null, null, err.errors);
+	}
 }
 
-async function valid_author_remove(req, res, next){        
-    const { manga_id } = req.query;       
+async function valid_author_remove(req, res, next) {
+	const { manga_id } = req.query;
 
-    if (mongoose.Types.ObjectId.isValid(manga_id)) {   
-        if (String(new mongoose.Types.ObjectId(manga_id)) === manga_id) {  
-            next();     
-        } else { 
-
-            return res.jsonBadRequest(
-                    null,
-                    getMessage("manga.invalid.id"),
-                    null);
-           
-        } 
-    } else {
-        return res.jsonBadRequest(
-                null,
-                getMessage("manga.invalid.id"),
-                null);         
-    }                     
-}  
+	if (isValidMongoId(manga_id)) {
+		next();
+	} else {
+		return res.jsonBadRequest(null, getMessage("manga.invalid.id"), null);
+	}
+}
 
 export default {
-    valid_author_store,
-    valid_author_read,
-    valid_author_list, 
-    valid_author_update,
-    valid_author_remove
-}
+	valid_author_store,
+	valid_author_read,
+	valid_author_list,
+	valid_author_update,
+	valid_author_remove,
+};
