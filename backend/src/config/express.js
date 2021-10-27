c83b3b71d5e2468e93f1ade84e7d805b48d3a308
WaifuForever <itachiut1r4@gmail.com>
2021-10-27 09:05:35 -0300

    add: express config file

---
+++ backend/src/config/express.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoute from "../routes/Authentication.js";
+import authorRoute from "../routes/Author.js";
+import chapterRoute from "../routes/Chapter.js";
+import mangaRoute from "../routes/Manga.js";
+import reviewRoute from "../routes/Review.js";
+import userRoute from "../routes/User.js";
+
+import corsOptionsDelegate from "./cors.js";
+import limiter from "./limiter.js";
+
+import { response } from "../middlewares/ResponseMiddleware.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/files", express.static("uploads"));
+
+app.use(cookieParser());
+app.use(cors(corsOptionsDelegate));
+app.use(limiter); // limiting all requests
+app.use(response);
+
+app.use(authRoute);
+app.use("/author", authorRoute);
+app.use("/chapter", chapterRoute);
+app.use("/manga", mangaRoute);
+app.use("/review", reviewRoute);
+app.use("/user", userRoute);
+
+export { app };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,12 @@
 import dotenv from "dotenv";
-
-import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import http from "http";
 //import io from 'socket.io';
 
-import { response } from "./middlewares/ResponseMiddleware.js";
-
-import corsOptionsDelegate from "./config/cors.js";
-import limiter from "./config/limiter.js";
-
-import authRoute from "./routes/Authentication.js";
-import authorRoute from "./routes/Author.js";
-import chapterRoute from "./routes/Chapter.js";
-import mangaRoute from "./routes/Manga.js";
-import reviewRoute from "./routes/Review.js";
-import userRoute from "./routes/User.js";
+import app from "./config/express.js";
 
 dotenv.config();
 
-const app = express();
 const server = http.Server(app);
 
 mongoose.connect(
@@ -32,22 +17,6 @@ mongoose.connect(
 		useFindAndModify: false,
 	}
 );
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/files", express.static("uploads"));
-
-app.use(cookieParser());
-app.use(cors(corsOptionsDelegate));
-app.use(limiter); // limiting all requests
-app.use(response);
-
-app.use(authRoute);
-app.use("/author", authorRoute);
-app.use("/chapter", chapterRoute);
-app.use("/manga", mangaRoute);
-app.use("/review", reviewRoute);
-app.use("/user", userRoute);
 
 server.listen(parseInt(`${process.env.PORT}`), () => {
 	console.log(`Listening on port ${process.env.PORT}`);
