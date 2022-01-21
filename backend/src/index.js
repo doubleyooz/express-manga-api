89f1ec1898469a2de3e4201b97a804fb7435ad6e
Jojo <sirjoandersonwf@gmail.com>
2022-01-21 09:50:31 -0300

    update: Db connection env variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,8 @@ import http from 'http';
 import { app } from './config/express.config.js';
 
 const server = http.Server(app);
-const str = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-c09yq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-mongoose.connect(str, {
+mongoose.connect(`${process.env.DB_CONNECTION}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
