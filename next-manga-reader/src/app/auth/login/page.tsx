32472ce750692d6ce7aa2139fa70dad4fe86655b
Joanderson <sirjoandersonwf@gmail.com>
2024-08-18 14:23:28 -0300

    refactor: button

--- next-manga-reader/src/app/auth/login/page.tsx
+++ next-manga-reader/src/app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import NextLink from "next/link";
+import Image from "next/image";
 import login from "./login";
 import { useForm } from "react-hook-form";
 import TextField from "@/components/TextField";
@@ -8,27 +9,35 @@ import { LoginFormProps, loginSchema } from "@/common/zod.common";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import Button from "@/components/Button";
+import { useState } from "react";
+import { CiCircleAlert } from "react-icons/ci";
 
 export default function Login() {
+  const [loading, setLoading] = useState(false);
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<LoginFormProps>({
     resolver: zodResolver(loginSchema),
   });
 
   const onSubmit = async (data: LoginFormProps) => {
+    setLoading(true);
     console.log(data);
 
     //await signIn(data.email, data.password);
 
     //nav("/");
+    setLoading(false);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-xs">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full max-w-xs px-10 py-20 rounded-[36px] bg-white"
+    >
       <div className="flex flex-col gap-2">
         <TextField
           name="email"
@@ -44,10 +53,73 @@ export default function Login() {
           error={errors["password"]?.message}
           register={register}
         />
-        <Button type="submit" text="Login" />
-        <NextLink className="self-center" href="/auth/signup">
-          Signup
+        <NextLink
+          className="font-sans self-end text-xs text-gray-400"
+          href="/auth/forgot"
+        >
+          Forgot Password?
         </NextLink>
+        <div className="flex flex-col w-full justify-center gap-8 mt-9">
+          <Button
+            type="submit"
+            text="Login"
+            variant="primary"
+            loading={loading}
+            disabled={!isValid}
+            rounded
+          />
+          <span className="text-center">OR</span>
+          <div className="flex justify-center gap-4">
+            <Button
+              type="submit"
+              variant="text"
+              loading={loading}
+              icon={
+                <Image
+                  src="/facebook.png"
+                  width={24}
+                  height={24}
+                  alt="Picture of the author"
+                />
+              }
+              rounded
+              outlined
+              shadow
+            />
+            <Button
+              type="submit"
+              variant="text"
+              loading={loading}
+              icon={
+                <Image
+                  src="/twitter.png"
+                  width={24}
+                  height={24}
+                  alt="Picture of the author"
+                />
+              }
+              rounded
+              outlined
+              shadow
+            />
+            <Button
+              type="submit"
+              variant="text"
+              loading={loading}
+              icon={
+                <Image
+                  src="/google.png"
+                  width={24}
+                  height={24}
+                  alt="Picture of the author"
+                />
+              }
+              rounded
+              outlined
+              shadow
+            />
+          </div>
+        </div>
       </div>
     </form>
   );
--- next-manga-reader/src/components/Button.tsx
+++ next-manga-reader/src/components/Button.tsx
@@ -1,30 +1,115 @@
 import type React from "react";
+import { ClipLoader } from "react-spinners";
+import type { IconType } from "react-icons"; // Import the IconType from react-icons
+import { createElement } from "react";
+
+type StyleVariant = "text" | "primary";
+type StyleSize = "x-small" | "small" | "medium" | "large";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   handleClick?: () => any;
   type: "button" | "submit" | "reset" | undefined;
-  text: string;
-  uppercase?: boolean;
+  text?: string;
+  size?: StyleSize;
+  variant?: StyleVariant;
+  outlined?: boolean;
+  loading?: boolean;
+  rounded?: boolean;
+  disabled?: boolean;
+  shadow?: boolean;
+  appendIcon?: React.ReactNode;
+  prependIcon?: React.ReactNode;
+  icon?: React.ReactNode;
 }
+const generateClassname = (
+  variant: StyleVariant,
+  size: StyleSize,
+  outlined: boolean = false,
+  rounded: boolean = false,
+  icon: boolean = false
+) => {
+  const base = "border ";
+  const mainColor =
+    variant === "primary"
+      ? "bg-blue-700 text-white border-blue-700"
+      : "text-gray-800 hover:bg-gray-100";
+  const border = outlined ? "" : " border-0";
+  const roundedClass = icon ? " p-2 rounded-full" : " w-full rounded-lg";
+  const roundedStyle = rounded
+    ? roundedClass.replace("rounded-lg", "rounded-3xl")
+    : roundedClass;
+  const padding = rounded ? "py-2 px-4" : "px-4";
+  const sizeMap = {
+    "x-small": {
+      height: "h-6 text-xs",
+      width: "min-w-6 max-w-6",
+    },
+    small: {
+      height: "h-7 text-xs",
+      width: "min-w-7 max-w-7",
+    },
+    medium: {
+      height: "h-9 text-sm",
+      width: "min-w-9 max-w-9",
+    },
+    large: {
+      height: "h-10 text-base",
+      width: "min-w-10 max-w-10",
+    },
+  };
+  const { height, width } = sizeMap[size] ?? sizeMap["medium"];
+
+  return `${base}${mainColor}${border}${roundedStyle} ${
+    icon ? `${height} ${width} ` : `${height} ${padding}`
+  }`;
+};
 
 const Button: React.FC<ButtonProps> = ({
   handleClick = () => {},
   text,
   type,
-  uppercase,
+  size = "medium",
+  variant = "text",
   disabled,
+  loading,
+  outlined,
+  shadow,
+  rounded,
+  appendIcon,
+  prependIcon,
+  icon,
 }) => {
+  const btnClasses = generateClassname(
+    variant,
+    size,
+    outlined,
+    rounded,
+    !!icon
+  );
+
   return (
     <button
       disabled={disabled}
       onClick={() => handleClick()}
-      className={`mt-10 font-bold transition duration-300 ease-in-out ${
-        uppercase && "uppercase"
-      } ${disabled && "text-dark-300"}`}
+      className={`w- text-base font-bold transition duration-300 ease-in-out  ${
+        shadow && "shadow-2xl shadow-black"
+      } ${disabled && "opacity-50 text-gray-300 "} ${btnClasses}`}
       aria-label={text}
       type={type}
     >
-      {text}
+      {loading ? (
+        <ClipLoader
+          color={outlined ? "#2563eb" : "#fffff"}
+          loading={loading}
+          size={40}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        <div className="flex justify-center items-center gap-2">
+          {prependIcon} {icon ? icon : text} {appendIcon}
+        </div>
+      )}
     </button>
   );
 };
